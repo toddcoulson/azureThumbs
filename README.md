0d05c660dab137e41022603b841c02244dc57bd7
toddcoulson <todd.coulson@americanreading.com>
2020-06-24 08:56:18 -0400

    adding images thumbnail files

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
-# azureThumbs
-Create thumbnails from uploaded jpg files. 
+# Azure Storage Blob Thumnail Creation on Upload #
+
+This function is triggered when someone uploads a file into bis blob on arcbisstore Storage Account in azure.
+
+### Tutorials ###
+
+The code for this project was acquired from:<br />
+<a href='https://github.com/Azure-Samples/storage-blob-resize-function-node-v10'>Github</a> <br />
+and<br />
+<a href='https://docs.microsoft.com/en-us/azure/event-grid/resize-images-on-storage-blob-upload-event?tabs=nodejsv10#prerequisites'>Microsoft Azure tutorial</a>
+
+
+### Crator ###
+
+* Todd Coulson
---
+++ Thumbnail/index.js
@@ -0,0 +1,82 @@
+const stream = require('stream');
+const Jimp = require('jimp');
+
+const {
+  Aborter,
+  BlobURL,
+  BlockBlobURL,
+  ContainerURL,
+  ServiceURL,
+  SharedKeyCredential,
+  StorageURL,
+  uploadStreamToBlockBlob
+} = require("@azure/storage-blob");
+
+const ONE_MEGABYTE = 1024 * 1024;
+const ONE_MINUTE = 60 * 1000;
+const uploadOptions = { bufferSize: 4 * ONE_MEGABYTE, maxBuffers: 20 };
+
+const containerName = process.env.BLOB_CONTAINER_NAME;
+const accountName = process.env.AZURE_STORAGE_ACCOUNT_NAME;
+const accessKey = process.env.AZURE_STORAGE_ACCOUNT_ACCESS_KEY;
+
+const sharedKeyCredential = new SharedKeyCredential(
+  accountName,
+  accessKey);
+const pipeline = StorageURL.newPipeline(sharedKeyCredential);
+const serviceURL = new ServiceURL(
+  `https://${accountName}.blob.core.windows.net`,
+  pipeline
+);
+const resizeIt = (widthInPixels, filename, context, inputBlob)=>{
+  const aborter = Aborter.timeout(30 * ONE_MINUTE);
+  const contentType = context.bindingData.data.contentType;
+  
+
+  Jimp.read(inputBlob).then( (thumbnail) => {
+
+    thumbnail.resize(widthInPixels, Jimp.AUTO);
+
+    const options = {
+      contentSettings: { contentType: contentType }
+    };
+
+    thumbnail.getBuffer(Jimp.MIME_PNG, async (err, buffer) => {
+
+      const readStream = stream.PassThrough();
+      readStream.end(buffer);
+
+      const containerURL = ContainerURL.fromServiceURL(serviceURL, containerName);
+      const blockBlobURL = BlockBlobURL.fromContainerURL(containerURL, blobName);
+
+      try {   
+
+        await uploadStreamToBlockBlob(aborter, readStream,
+          blockBlobURL, uploadOptions.bufferSize, uploadOptions.maxBuffers,
+          { blobHTTPHeaders: { blobContentType: "image/jpeg" } });
+
+      } catch (err) {
+
+        context.log(err.message);
+
+      } finally {        
+
+        context.done();
+
+      }
+    });
+  });
+}
+
+module.exports = (context, eventGridEvent, inputBlob) => {  
+  const blobUrl = context.bindingData.data.url;
+  const blobName = blobUrl.slice(blobUrl.lastIndexOf("/")+1, -4);
+  let filename = blobName + '-240.jpg';
+  resizeIt(240, filename, context, inputBlob);
+
+  filename = blobName + '-48.jpg';
+  resizeIt(48, filename, context, inputBlob);
+
+  filename = blobName + '-32.jpg';
+  resizeIt(32, filename, context, inputBlob);
+};
